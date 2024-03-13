779ec9c90ea9956535c3724e639aaa037a133135
Charlotte Wong <chiharutaru@gmail.com>
2024-03-13 16:00:00 -0700

    LRCI-4090 debug code to test if class is running properly

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/TestClassGroupFactory.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/TestClassGroupFactory.java
@@ -337,6 +337,8 @@ public class TestClassGroupFactory {
 				}
 			}
 			else if (batchName.startsWith("modules-semantic-versioning-")) {
+				System.out.println("Charw inside modules-semantic-versioning logic");
+
 				if (jsonObject != null) {
 					batchTestClassGroup = new SemVerModulesBatchTestClassGroup(
 						jsonObject, portalTestClassJob);
@@ -428,6 +430,10 @@ public class TestClassGroupFactory {
 			}
 			else if (batchName.startsWith("semantic-versioning")) {
 				if (jsonObject != null) {
+
+					System.out.println("Charw batchName is: " + batchName);
+					System.out.println("Charw inside of semantic-versioning logic");
+
 					batchTestClassGroup = new SemanticVersioningTestClassGroup(
 						jsonObject, portalTestClassJob);
 				}
