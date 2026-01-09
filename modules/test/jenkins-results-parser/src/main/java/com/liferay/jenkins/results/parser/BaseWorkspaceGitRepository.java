f898ad87ea292cd689424391617fae67912d1c7f
Charlotte Wong <chiharutaru@gmail.com>
2026-01-09 10:15:58 -0800

    LRCI-6626 error msg

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseWorkspaceGitRepository.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseWorkspaceGitRepository.java
@@ -554,14 +554,14 @@ public abstract class BaseWorkspaceGitRepository
 		}
 
 		if (_isPullRequest()) {
-
 			System.out.println("@@ cw is pull request");
 
 			try {
 				_localGitBranch = _createPullRequestLocalGitBranch();
 			}
 			catch (Exception exception) {
-				System.out.println("@@ cw failed to create pull request branch");
+				System.out.println(
+					"@@ cw failed to create pull request branch");
 				System.out.println(exception);
 			}
 		}
@@ -574,7 +574,10 @@ public abstract class BaseWorkspaceGitRepository
 			catch (Exception exception) {
 				System.out.println(
 					"@@ cw failed to create remote git ref branch");
-				System.out.println(exception);
+
+				throw new RuntimeException(
+					exception +
+						"failed to create local git branch, check parameters");
 			}
 		}
 
@@ -772,8 +775,6 @@ public abstract class BaseWorkspaceGitRepository
 
 			if (JenkinsResultsParserUtil.isNullOrEmpty(remoteGitBranchSHA) ||
 				!gitWorkingDirectory.localSHAExists(remoteGitBranchSHA)) {
-
-
 
 				continue;
 			}
