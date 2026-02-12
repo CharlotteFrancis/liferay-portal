b1ccf46f71001bbc5254fd55eacc39953833c9ac
Charlotte Wong <chiharutaru@gmail.com>
2026-02-12 09:56:08 -0800

    LRCI-6522 return correct build profile

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/SanitizeLanguageTopLevelBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/SanitizeLanguageTopLevelBuild.java
@@ -39,6 +39,11 @@ public class SanitizeLanguageTopLevelBuild
 	}
 
 	@Override
+	public Job.BuildProfile getBuildProfile() {
+		return Job.BuildProfile.DXP;
+	}	
+
+	@Override
 	public PortalWorkspace getPortalWorkspace() {
 		Workspace workspace = getWorkspace();
 
@@ -87,7 +92,7 @@ public class SanitizeLanguageTopLevelBuild
 			System.out.println(
 				"@@ cw in SanitizeLanguageTopLevelBuild aftr portalWorkspace");
 
-			portalWorkspace.setBuildProfile(Job.BuildProfile.DXP);
+			portalWorkspace.setBuildProfile(getBuildProfile());
 		}
 
 		System.out.println("@@ cw workspace build profile set");
